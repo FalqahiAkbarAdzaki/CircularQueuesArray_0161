979ae86205470384df45ee12f0c7b4abce89300b
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-05-30 11:32:01 +0700

    30

--- CircularQueuesArray_0161.cpp
+++ CircularQueuesArray_0161.cpp
@@ -108,3 +108,17 @@ int main() {
 			cout << "2. Implement selete operation" << endl;
 			cout << "3. Display values" << endl;
 			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}

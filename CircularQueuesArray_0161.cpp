aa1e44a31f6f07a968523a19551e81514e93bce8
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-05-30 11:17:04 +0700

    21

--- CircularQueuesArray_0161.cpp
+++ CircularQueuesArray_0161.cpp
@@ -81,4 +81,15 @@ public:
 		else {
 			// Jika FRONT > REAR, iterasi dari FRONT hingga akhir array
 			while (FRONT <= max - 1) {
-			};
+				cout << queue_array[Front] << "  ";
+				FRONT++;
+			}
+
+			FRONT = 0;
+
+			// Iterasi dari awal array hingga REAR
+			while (FRONT <= REAR) {
+				cout << queue_array[FRONT] << "  ";
+				FRONT++;
+			}
+};
